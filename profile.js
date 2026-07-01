5a9f7370bd4cccd49dea84c336a2e785c2ff461f
MahathiKanigiri006 <mahathikanigiri@gmail.com>
2026-07-01 10:07:06 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,200 @@
+const members = {
+
+1:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 16-01-33.png",
+
+name:"Kiran Kumar",
+
+role:"Chairman & Managing Director",
+
+relation:"Father",
+
+age:"50",
+
+education:"MBA",
+
+occupation:"Business Leader",
+
+experience:"25+ Years",
+
+email:"kiran@gmail.com",
+
+phone:"+91 9876543210",
+
+description:"Founder of Kanigiri Group of Companies. With over two decades of leadership experience, he has successfully guided the family organization through growth, innovation, and excellence."
+
+},
+
+2:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 15-41-03.png",
+
+name:"Sandhya",
+
+role:"Director of Operations",
+
+relation:"Mother",
+
+age:"48",
+
+education:"M.Sc",
+
+occupation:"Operations Manager",
+
+experience:"20+ Years",
+
+email:"sandhya@gmail.com",
+
+phone:"+91 9876543211",
+
+description:"Responsible for planning, administration and ensuring smooth operations of the family organization."
+
+},
+
+3:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 16-05-08.png",
+
+name:"Mahesh",
+
+role:"Business Development Executive",
+
+relation:"Brother",
+
+age:"23",
+
+education:"B.Tech",
+
+occupation:"Student",
+
+experience:"2 Years",
+
+email:"rohith@gmail.com",
+
+phone:"+91 9876543212",
+
+description:"A passionate learner interested in business management and digital technologies."
+
+},
+
+4:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 15-53-52.png",
+
+name:"Mahathi",
+
+role:"Technology & Innovation Lead",
+
+relation:"Daughter",
+
+age:"25",
+
+education:"B.Tech (CSE)",
+
+occupation:"Student",
+
+experience:"4 years",
+
+email:"mahathi@gmail.com",
+
+phone:"+91 9876543213",
+
+description:"Passionate about Web Development, Full Stack Technologies, Cyber Security and Post-Quantum Cryptography research."
+
+},
+
+5:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 16-07-24.png",
+
+name:"Lakshmi",
+
+role:"Family Advisor",
+
+relation:"Grandmother",
+
+age:"72",
+
+education:"Graduate",
+
+occupation:"Homemaker",
+
+experience:"40+ Years",
+
+email:"lakshmi@gmail.com",
+
+phone:"+91 9876543214",
+
+description:"Provides guidance, values and wisdom that strengthen the unity and traditions of the family."
+
+},
+
+6:{
+
+photo:"/home/user/Screenshots/Screenshot from 2026-06-22 16-09-52.png ",
+
+name:"Venkata Rao",
+
+role:"Senior Advisor",
+
+relation:"Grandfather",
+
+age:"75",
+
+education:"B.A.",
+
+occupation:"Retired Government Officer",
+
+experience:"35+ Years",
+
+email:"ramarao@gmail.com",
+
+phone:"+91 9876543215",
+
+description:"A retired government officer known for discipline, integrity and decades of public service."
+
+}
+
+};
+
+
+// Get ID from URL
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+const person = members[id];
+
+// Display Data
+
+document.getElementById("photo").src = person.photo;
+
+document.getElementById("name").innerHTML = person.name;
+
+document.getElementById("role").innerHTML = person.role;
+
+document.getElementById("relation").innerHTML =
+"<strong>Relationship :</strong> " + person.relation;
+
+document.getElementById("age").innerHTML =
+"<strong>Age :</strong> " + person.age;
+
+document.getElementById("education").innerHTML =
+"<strong>Education :</strong> " + person.education;
+
+document.getElementById("occupation").innerHTML =
+"<strong>Occupation :</strong> " + person.occupation;
+
+document.getElementById("experience").innerHTML =
+"<strong>Experience :</strong> " + person.experience;
+
+document.getElementById("email").innerHTML =
+"<strong>Email :</strong> " + person.email;
+
+document.getElementById("phone").innerHTML =
+"<strong>Phone :</strong> " + person.phone;
+
+document.getElementById("description").innerHTML =
+"<strong>About :</strong> " + person.description;
